9c7eed80b2b9736a1bf86c508dad338241ab3b95
dasosis <soumyasnigdhadas@gmail.com>
2022-12-07 01:01:58 +0530

    complete2

--- pcb.c
+++ pcb.c
@@ -39,9 +39,24 @@ void display(struct node* head)
         printf("Empty Q\n");
         return;
     }
-    printf("PNO Bound(CPU/IO)\n");
+    printf("Process Number\tCPU Burst\tIO Burst\tTotal Burst\tStatus(CPU/IO)\n");
     while(head!=NULL) {
-        printf("%d %d %d %d %d\n",head->pno,head->cpuBT,head->ioBT,head->totalBT,head->status);
+        printf("      %d\t   %d\t\t   %d\t\t     %d\t\t     %d\n",head->pno,head->cpuBT,head->ioBT,head->totalBT,head->status);
+        head=head->next;
+    }printf("\n");
+    return;
+}
+
+void pr_display(struct node* head)
+{
+    if(head == NULL)
+    {
+        printf("Empty Q\n");
+        return;
+    }
+    printf("Process Number\tCPU Burst\tIO Burst\tTotal Burst\tStatus(CPU/IO)\tPriority\n");
+    while(head!=NULL) {
+        printf("      %d\t   %d\t\t   %d\t\t     %d\t\t     %d\t\t    %d\n",head->pno,head->cpuBT,head->ioBT,head->totalBT,head->status,head->pr);
         head=head->next;
     }printf("\n");
     return;
@@ -105,38 +120,6 @@ void MergeSort(struct node** headRef)
 	*headRef = SortedMerge(a, b);
 }
 
-
-
-
-/*
-int* pop(struct node** head, int *x) 
-{
-    if (*head == NULL) {
-        printf("Empty Q pop\n");
-        return 0;
-    }
-    if((*head)->next == NULL)
-    {
-        x[0] = (*head)->pno;
-        x[1] = (*head)->bound;
-        free(*head);
-        *head = NULL;
-        return x;
-    }
-    struct node* temp = *head;
-    struct node* prev = *head;
-    while(temp->next!=NULL) {
-        prev = temp;
-        temp=temp->next;
-    }
-    x[0] = temp->pno;
-    x[1] = temp->bound;
-    prev->next = NULL;
-    free(temp);
-    return x;
-}*/
-
-
 void assignPR(struct node** head,int n)
 {
     if (*head == NULL) {
@@ -149,41 +132,25 @@ void assignPR(struct node** head,int n)
         return;
     }
     struct node* temp = *head;
-    struct node* prev = *head;
-
-    for (int i = 0; i < n && temp->next!=NULL; i++)
+    int pr_count = 0;
+    while (temp!=NULL)
     {
-        if(temp->totalBT == (temp->next)->totalBT)
+        temp->pr = pr_count;
+        if(temp->next!=NULL)
         {
-            
+            if(temp->totalBT != (temp->next)->totalBT)
+                pr_count++;
         }
-    }
-    
-    while(temp->next!=NULL) {
-        prev = temp;
-        temp=temp->next;
-    }
-    x[0] = temp->pno;
-    x[1] = temp->bound;
-    prev->next = NULL;
-    free(temp);
-    return x;
+        temp = temp->next;
+    }    
 }
-
 
 int main()
 {
     struct node* inq = NULL;
-    //struct node* outq = NULL;
     int n, total=0, cpu = 2, io = 0, status;
-    //int *a;
     printf("enter number of processes - ");
     scanf("%d",&n);
-
-
-
-
-    
 
     srand(time(0));
     for(int i=0;i<n;i++)
@@ -197,34 +164,8 @@ int main()
     printf("\n/Input Queue/\n");
     display(inq);
     MergeSort(&inq);
-    display(inq);
     assignPR(&inq, n);
-    /*for (int i = 0; i < n;)
-    {
-        while(count < 2)
-        {   
-            a = pop(&inq,P);
-            i++;
-            if(a[1]==1)
-            {
-                push(&outq,a[0],a[1]);
-                count++;
-            }
-        }
-        a = pop(&inq,P);
-        i++;
-        if (a[1] == 1)
-        {
-            push(&outq,a[0],a[1]);
-            cpu++;
-        }
-        if (a[1]==0 && cpu>io)
-        {
-            push(&outq,a[0],a[1]);
-            io++;
-        }
-    }
     printf("\n/Output Queue/\n");
-    display(outq);   */
+    pr_display(inq);
 }
 
